6ed648551099d1c1c1547ca0831661bccf854ccc
intelligentgamer <39501953+intelligentgamer@users.noreply.github.com>
2018-08-28 16:48:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const db = low(adapter);
 
 db.defaults({ histoires: [], xp: []}).write();
 var prefix = "ub!!";
-client.login("NDUwNDQ5NDMzMzQ0MzQ0MDY0.DmbQ0A.NlmjDLx4_yzNAMD4b7WiVnKN-r8");
+client.login(process.env.TOKEN);
 
 client.on('message', message => {
     var msgauthor = message.author.id;
